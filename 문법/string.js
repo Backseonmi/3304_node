c3a70d5d2bff2d3245a3b1c6c68c0e07000930e6
Backseonmi <w2102@e-mirim.hs.kr>
2023-04-19 09:50:35 +0900

    문자열 기초

---
+++ 문법/string.js
@@ -0,0 +1,23 @@
+const str = "Hello world";
+
+console.log(str.length);  //11(띄어쓰기 포함)
+
+//문자열 쪼개기(본체 변화x)
+console.log(str.slice(1,5));  //'ello'
+console.log(str.slice(1));  //'ello world'
+console.log(str.slice(-2));  //'ld'
+
+//문자열 대체(본체 변화x)
+a = str.replace('world', 'everyone');  //'Helllo everyone'
+console.log(a);
+
+//대소문자 변환(본체 변화x)
+console.log(str.toUpperCase());  //"HELLO WORLD"
+console.log(str.toLowerCase());  //"hello world"
+
+//문자열 포함여부
+console.log(str.includes('world')); //true
+console.log(str.includes('hello')); //flase(h가 대문자 이어야 함)
+
+
+console.log(str);
